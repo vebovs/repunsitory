4f8427a8de480714c2424ba4a4516155ca5ef8be
vebovs <vebovs@hotmail.no>
2020-03-23 21:31:09 +0100

    updated email verification link

--- config/frontend.php
+++ config/frontend.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'url' => env('FRONTEND_URL', 'http://localhost:8000'),
+    'url' => env('FRONTEND_URL', 'https://repunsitory.herokuapp.com'),
     // path to my frontend page with query param queryURL(temporarySignedRoute URL)
     'email_verify_url' => env('FRONTEND_EMAIL_VERIFY_URL', '/verify-email?queryURL='),
 ];
